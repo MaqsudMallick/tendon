f78dc4f350f5a253979145e73449a7b4ab4db1d4
MaqsudMallick <maqsudmallick@gmail.com>
2026-09-11 19:32:17 +0530

    chore(testcontainers): add @ServiceConnection bean

--- src/test/java/com/devnine/tendon/TestcontainersConfiguration.java
+++ src/test/java/com/devnine/tendon/TestcontainersConfiguration.java
@@ -1,8 +1,17 @@
 package com.devnine.tendon;
 
 import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.postgresql.PostgreSQLContainer;
+import org.testcontainers.utility.DockerImageName;
 
 @TestConfiguration(proxyBeanMethods = false)
 class TestcontainersConfiguration {
+    @Bean
+    @ServiceConnection
+    PostgreSQLContainer postgreContainer() {
+        return new PostgreSQLContainer(DockerImageName.parse("postgres:16"));
+    }
 
 }
